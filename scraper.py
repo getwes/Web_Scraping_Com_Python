4d9b24e942c0af35c278a76318a0c3630a3f353a
Wesley Freire <wesleyfreire1707@gmail.com>
2023-04-08 14:29:09 -0300

    atualização

--- scraper.py
+++ scraper.py
@@ -14,16 +14,27 @@ headers = {'User-Agente': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 site = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(site.content, 'html.parser')#basicamente vou estar analizando o conteudo do site
-placas = soup.find_all('div' , class_='MuiCardContent-root jss62' )
+#placas = soup.find_all('div' , class_='MuiCardContent-root jss62' ) # div onde passa os dados, nome,preço,parcela
 ultima_pagina= soup.find('button', class_='MuiButtonBase-root MuiPaginationItem-root MuiPaginationItem-root')
 
-placa = placas[0] # existe varias placas, estou pedidindo apenas uma para ser analizada
-marca = placa.find('h2', class_='MuiTypography-root jss76 jss77 MuiTypography-h6').get_text().strip()#.get_text() para filtrar apenas a informação de texto
-preco = placa.find('div', class_='jss87').get_text().strip()
+for i in range(1,int(ultima_pagina)):
+    url_pag = f'https://www.pichau.com.br/hardware/placa-de-video?page={i}2'
+    site = requests.get(url_pag, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    placas = soup.find_all('div' , class_='MuiCardContent-root jss62' )
+
+    with open ('precos_placas.csv', 'a', newline='',encoding='UTF-8') as f:
+        for placa in placas:
+            marca = placa.find('h2', class_='MuiTypography-root jss76 jss77 MuiTypography-h6').get_text().strip()
+
+placa = placas[0] # existe varias placas, estou pedindo apenas uma para ser analizada
+
+#filtrando os valores importante
+
 num_preco = preco[2:] #para tirar o R$ estou pedindo o valor depois do indice dois
 num_preco = num_preco[:-3] # para tirar os centavos
 
 preco_avista = placa.find('div', class_='jss79').get_text().strip()
-num_avista = preco_avista[2:]
-num_avista = num_avista[:-3]
-print(num_avista)
+index = preco_avista.rfind('.') #pedindo os dados antes do index (,)
+num_preco_avista = preco_avista[2:index]
+print(num_preco_avista)
